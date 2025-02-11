a6027c1113366972b6dcdae79c55e999fff0ef3c
Lily Sun <143292908+lilyrsun@users.noreply.github.com>
2025-02-10 18:11:04 -0600

    tilemap stuff

--- Assets/Scripts/TowerSpawner.cs
+++ Assets/Scripts/TowerSpawner.cs
@@ -1,35 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class TowerSpawner : MonoBehaviour
 {
-    // Assign tower prefab
     public GameObject towerPrefab;
-
-    // X coordinate of the tile to spawn on
     public int tileX = 2;
-
-    // Y coordinate of the tile to spawn on
     public int tileY = 3;
 
     void Start()
     {
-        // tile name using the same format from GridManager
+        // Tile name that the script is looking for
         string tileName = $"Tile {tileX} {tileY}";
+        Debug.Log("Looking for tile: " + tileName);
 
-        GameObject targetTile = GameObject.Find(tileName);
+        // Find all Tile objects in the scene
+        Tile[] allTiles = FindObjectsOfType<Tile>();
+
+        Debug.Log("Total tiles found: " + allTiles.Length);  // Should be >0 if tiles exist
+
+        GameObject targetTile = null;
+
+        foreach (Tile tile in allTiles)
+        {
+            Debug.Log("Checking tile: " + tile.gameObject.name);
+            if (tile.gameObject.name == tileName)
+            {
+                targetTile = tile.gameObject;
+                Debug.Log("Found matching tile!");
+                break;
+            }
+        }
 
         if (targetTile != null)
         {
-            Instantiate(towerPrefab, targetTile.transform.position, Quaternion.identity);
+            // Adjust spawn position to make sure it's visible
+            Vector3 spawnPosition = targetTile.transform.position; // Lift & adjust z-position
+
+            // Spawn tower with increased scale
+            GameObject tower = Instantiate(towerPrefab, spawnPosition, Quaternion.identity);
+            //tower.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f); // Make sure it's visible
+
+            // Corrected Debug.Log Statement
             Debug.Log("Tower spawned on " + tileName);
         }
-        else
-        {
-            Debug.LogError("Tile not found: " + tileName);
-        }
-
     }
-}
+} // <-- Closing brace for TowerSpawner class
